3dcb028395664a6221db52fc73d4959d3e04b63e
Levirbss <levi.serrano1204@gmail.com>
2025-03-19 14:50:14 -0300

    teste
    ta passando aqui, mas no git action nao, quero analisar melhor o erro

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,16 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest', // Adicione esta linha para suportar ES Modules
   },
   transformIgnorePatterns: [
-    '/node_modules/',
+    '/node_modules/(?!@aws-sdk|mime)', // Adicione esta linha para transformar o módulo mime
   ],
   moduleNameMapper: {
-    '@config/(.*)': '<rootDir>/src/config/$1',
-    '@modules/(.*)': '<rootDir>/src/modules/$1',
-    '@shared/(.*)': '<rootDir>/src/shared/$1',
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@shared/(.*)$": "<rootDir>/src/shared/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^@routeParams/(.*)$": "<rootDir>/src/routeParams/$1",
   },
 };
