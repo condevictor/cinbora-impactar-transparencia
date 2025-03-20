75e17ae671fe4c0b94bae2fbf124511c345e4ffc
Levirbss <levi.serrano1204@gmail.com>
2025-03-20 18:29:12 -0300

    teste

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,17 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const transform = {
-  '^.+\\.tsx?$': 'ts-jest',
-  '^.+\\.jsx?$': 'babel-jest',
-};
-export const transformIgnorePatterns = [
-  '/node_modules/(?!@aws-sdk|mime)',
-];
-export const moduleNameMapper = {
-  "^@modules/(.*)$": "<rootDir>/src/modules/$1",
-  "^@shared/(.*)$": "<rootDir>/src/shared/$1",
-  "^@config/(.*)$": "<rootDir>/src/config/$1",
-  "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
-  "^@routeParams/(.*)$": "<rootDir>/src/routeParams/$1",
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@aws-sdk|mime)',
+  ],
+  moduleNameMapper: {
+    "^@modules/(.*)$": "<rootDir>/src/modules/$1",
+    "^@shared/(.*)$": "<rootDir>/src/shared/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
+    "^@routeParams/(.*)$": "<rootDir>/src/routeParams/$1",
+  },
 };
